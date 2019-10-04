c4c2c35c1b8dc749b895f5702c6144074ff1f158
William St. Onge <stongew1@mymail.nku.edu>
2019-10-04 11:44:31 -0400

    Fixed comments and removed temp variable (replaced with register ebx)

--- Assignment5/selectionsort.c
+++ Assignment5/selectionsort.c
@@ -6,49 +6,58 @@
 
 int main()
 {
-	int data[] = { 5, 8, 9, 4, 2, 1, 3, 6, 10, 7 };
+	int data[] = { 8, -1, 7, -8, 6, 3, -4, 4, 1, -2, 9, -5, 0, 2};
 	int lengths = sizeof(data);
 
-	int min, minpos, i = 0, j, temp;
+	int min, minpos, i = 0, j;
 
 	__asm {
-	outer:								; Outer for loop (first part)
-		mov eax, i						; eax = i
-		mov dword ptr[minpos], eax		; minpos = i
-		mov ebx, data[eax]				; min = data[i]
-		mov dword ptr[min], ebx
-		add eax, 4						; eax += 4
+	outer:							; Outer loop 
 
-	inner:								; Inner for loop
-		mov dword ptr[j], eax			; j = eax (next element in array)
-		mov ebx, data[eax]				; if(data[j] < data[minpos])
+		mov eax, i					; Initialize the loop
+
+		mov minpos, eax				; Setup min and minpos
+		mov ebx, data[eax]				
+		mov min, ebx
+
+		add eax, 4					; Increment the array index by 4 (int is 4b wide)
+
+	inner:							; Inner loop
+
+		mov j, eax					; Set j equal to the element after i
+
+		mov ebx, data[eax]				; Check if the data at j is less then that at minpos
 		mov ecx, minpos
 		cmp ebx, data[ecx]
-		jge outer_l						; if this above condition is true, skip the stuff in the if condition
-		mov min, ebx					; min = data[j]
-		mov minpos, eax					; minpos = j
+		jge outer_l					; If it is greater than or equal to, jump to past the condition 
 
-	outer_l:							; Swapping and continuing or stopping the loop if the sort is complete
-		add eax, 4						; j++ (for loop increment)
-		cmp eax, lengths				; Make sure j is less than the length of the array
+		mov min, ebx					; Move minpos and min ahead since a new smallest element was found
+		mov minpos, eax					
+
+	outer_l:						; Swapping and continuing or stopping the loop if the sort is complete
+
+		add eax, 4					; Increment j
+
+		cmp eax, lengths				; If j is less than the length of the area, continue the loop
 		jl inner
-										; Swapping indices min and i's values
-		mov edx, i						; temp = data[i]
+
+		mov edx, i					; Swap data at indices min and i
 		mov ecx, data[edx]
-		mov dword ptr[temp], ecx
-		mov edx, minpos					; data[i] = data[minpos]
+		mov ebx, ecx
+		mov edx, minpos					
 		mov ecx, data[edx]
 		mov edx, i
 		mov data[edx], ecx
-		mov ecx, temp					; data[minpos] = temp
+		mov ecx, ebx					
 		mov edx, minpos
 		mov data[edx], ecx
 
-		mov edx, i						; i++ (for loop increment)
+		mov edx, i				; Increment i by 4
 		add edx, 4
 		mov i, edx
-		add edx, 4
-		cmp edx, lengths				; Make sure i is less than the length of the array
+
+		add edx, 4				; If i is less than the length of the array, continue the loop until it isn't
+		cmp edx, lengths				
 		jl outer
 	}
 
@@ -57,3 +66,7 @@ int main()
 	for (int n = 0; n < lengths / 4; n++)
 		printf("%d ", data[n]);
 }
+
+
+// Run 1 Output: 12 29 31 39 43 44 50 63 69 74 77 81 83 86 87 90 95 97 99 100
+// Run 2 Output: -8 -5 -4 -2 -1 0 1 2 3 4 6 7 8 9
