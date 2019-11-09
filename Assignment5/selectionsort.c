e775a18cd27173732e507c605ba9a35b9b5a471e
William St. Onge <stongew1@mymail.nku.edu>
2019-11-08 19:41:16 -0500

    communist

--- Assignment5/selectionsort.c
+++ Assignment5/selectionsort.c
@@ -20,62 +20,42 @@ int main()
 	int minpos;
 
 	__asm {
-		mov eax, 0						; Set i equal to 0
+		mov eax, 0						// Initialize the loop expression to 0 to start the loop
 		mov i, eax
-
-	outer:								; Outer loop.
-
-		mov eax, i						; Initialize the loop expression.
-
-		mov minpos, eax					; Set minpos equal to i and min equal to the element at i.
+	outer:								
+		mov eax, i						// Move the loop expression into eax so we can cmp and add
+		mov minpos, eax					// Set minpos equal to i and min equal to the element at i so we have base values for the inner loop.
 		mov ebx, data[eax]				
 		mov min, ebx
-
-		add eax, 4						; Increment the array index by 4 (int is 4b wide).
-
-	inner:								; Inner loop.
-
-		mov j, eax						; Set j equal to the element after i.
-
-		mov ebx, data[eax]				; Check if the data at j is less then that at minpos.
+		add eax, 4						// Increment the array index by 4 (ints are 4 bytes wide) to go to the next integer.
+	inner:							
+		mov j, eax						// Set j equal to the element after i.
+		mov ebx, data[eax]				// Check if the data at j is less then that at minpos to see if we need to modify min and minpos.
 		mov ecx, minpos
 		cmp ebx, data[ecx]
-		jge outer_l						; If it is greater than or equal to, jump past the condition.
-
-		mov min, ebx					; Move minpos and min ahead since a new smallest element was found.
+		jge outer_l						// If the above statement is greater than or equal to, jump past the condition since the next number is not smaller than min.
+		mov min, ebx					// Move minpos and min ahead since a new smallest element was found.
 		mov minpos, eax					
-
-	outer_l:							; Swapping and continuing or stopping the loop if the sort is complete.
-
-		add eax, 4						; Increment j.
-
-		cmp eax, lengths				; If j is less than the length of the area, continue the loop.
-		jl inner
-
-										; Swaps elements at indices i and minpos.
-		mov edx, i						; Move the index into edx, get the edx-th array element, then put the result into ebx.
+	outer_l:							
+		add eax, 4						// Increment j so we can test the next element.
+		cmp eax, lengths				// If j is less than the length of the area, continue the loop.
+		jl inner			
+		mov edx, i						// Swaps elements at indices i and minpos. Since minpos contains the current smaller element in the range and i is the first unsort element.
 		mov ebx, data[edx]
-
-		mov edx, minpos					; Move the minpos index into edx, get the edx-th array element, then put it into ecx.
+		mov edx, minpos					
 		mov ecx, data[edx]
-
-		mov edx, i						; Move the index into edx, move ecx to the edx-th position in the array.
+		mov edx, i						
 		mov data[edx], ecx
-
-		mov edx, minpos					; Move the minpos index into edx, move ebx to the edx-th position in the array.
+		mov edx, minpos				
 		mov data[edx], ebx
-
-		mov edx, i						; Increment i by 1 position (integers are 4 bytes wide).
+		mov edx, i						// Increment i by 1 position (integers are 4 bytes wide).
 		add edx, 4
 		mov i, edx
-
-		add edx, 4						; If i + 4 is less than the length of the array, continue the loop. If not, leave the inline assembly.
+		add edx, 4						// If i + 4 is less than the length of the array, continue the loop since were still inside of the array.
 		cmp edx, lengths				
 		jl outer
 	}
-
-
-	// Print out the values of the array
+	// Print out the values of the sorted array
 	for (int n = 0; n < lengths / 4; n++)
 		printf("%d ", data[n]);
 }
